b790fa3d6e6e05cf5a1bb8ae06f97573576a580c
agusrosa <agusrosa>
2022-08-26 23:33:50 -0300

    ajustes

--- transacciones.py
+++ transacciones.py
@@ -5,10 +5,5 @@ with open("eventos_classic.json") as archivo:
 estadoLISTA = []
 lista=datos["transacciones"]
 
-with open("eventos_black.json") as archivo:
-    datos = json.load(archivo)
-estadoLISTA = []
-listaBLACK=datos["transacciones"]
-
 for i in range(len(datos)):
     estadoLISTA.append(datos["transacciones"][i]["estado"])
