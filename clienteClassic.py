36e4b58a51753be37c96165d8d71734a58845b2d
agusrosa <agusrosa>
2022-09-04 12:56:16 -0300

    cambio datos clientes

---
+++ clienteClassic.py
@@ -0,0 +1,24 @@
+import json
+from clientes import Cliente
+
+
+class Classic(Cliente):
+
+    def __init__(self) -> None:
+        with open("data/eventos_classic.json") as jsonFile:
+            archivo = json.load(jsonFile)
+            jsonFile.close()
+            super().__init__(archivo, {'limite_extraccion_diario': 10000, 'limite_transferencia_recibida': 150000,
+                                       'costo_transferencia': 1})
+
+    def puede_crear_chequera(self, cant_chequeras) -> bool:
+        return False
+
+    def puede_crear_tarjeta_de_credito(self, cant_tarjetas) -> bool:
+        return False
+
+    def puede_comprar_dolar(self) -> bool:
+        return False
+
+    def posee_cuenta_corriente(self) -> bool:
+        return True
